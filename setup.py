540d894aadb2a85e454cfb054e7b48ecd9e8e9fd
Gari Araolaza <garaolaza@codesyntax.com>
2012-10-03 11:50:45 +0200

    Added metadata

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.8'
 
 setup(name='arartekomaps',
       version=version,
-      description="",
+      description="Django application serving mapak.ararteko.net ",
       long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
+      keywords='maps',
+      author='Gari Araolaza',
+      author_email='garaolaza@codesyntax.com',
+      url='https://github.com/ararteko/arartekomaps',
+      license='GPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
