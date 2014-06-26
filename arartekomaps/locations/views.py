9fad039df95bc0804c71db4c0988d178d525a208
urtzai <uodriozola@codesyntax.com>
2014-06-26 12:56:28 +0200

    Pagination bugfix

--- arartekomaps/locations/views.py
+++ arartekomaps/locations/views.py
@@ -73,7 +73,10 @@ def location(request,state,city,maincat='',subcat=''):
 
     prev_pars = request.GET.copy()
     next_pars = request.GET.copy()
-    prev_pars.update({'page':thispage.previous_page_number()})
+    if pagenumber == '1':
+        prev_pars.update({'page':'1'})
+    else:
+        prev_pars.update({'page':thispage.previous_page_number()})
     next_pars.update({'page':thispage.next_page_number()})
     prev_url = "/filter/?%s" % "&".join(["%s=%s" % (k,v) for k,v in prev_pars.items()])
     next_url = "/filter/?%s" % "&".join(["%s=%s" % (k,v) for k,v in next_pars.items()])
