aa6de81b329a5c8ee622e2b6a8ac90b886e34dbb
urtzai <uodriozola@codesyntax.com>
2014-11-17 17:04:12 +0100

    Prepare arartekomaps 5.31.

--- arartekomaps/places/translation.py
+++ arartekomaps/places/translation.py
@@ -2,6 +2,6 @@ from modeltranslation.translator import translator, TranslationOptions
 from arartekomaps.places.models import Place
 
 class PlaceTranslationOptions(TranslationOptions):
-    fields = ('description','url_name','url','adescription')
+    fields = ('description','url','adescription')
 
 translator.register(Place, PlaceTranslationOptions)
