f19eccbb736324b0f2c857bd559f4313f48b1388
urtzai <uodriozola@codesyntax.com>
2015-01-23 13:46:11 +0100

    Back to development: 5.42

--- arartekomaps/categories/translation.py
+++
@@ -1,7 +0,0 @@
-from modeltranslation.translator import translator, TranslationOptions
-from arartekomaps.places.models import Category
-
-class CategoryTranslationOptions(TranslationOptions):
-    fields = ('name',)
-
-translator.register(Category, CategoryTranslationOptions)
