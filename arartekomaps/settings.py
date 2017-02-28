77ae7b0ee276cead86b3fdf1adf7be31d429c380
Urtzi Odriozola <uodriozola@codesyntax.com>
2017-02-28 16:21:48 +0100

    Preparing release 5.85

--- arartekomaps/settings.py
+++ arartekomaps/settings.py
@@ -72,7 +72,7 @@ TRANSMETA_LANGUAGES = LANGUAGES
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/csmant/django/ararteko/media/'
+MEDIA_ROOT = '/home/csmant/django/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -83,7 +83,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/csmant/django/ararteko/static/'
+STATIC_ROOT = '/home/csmant/django/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
